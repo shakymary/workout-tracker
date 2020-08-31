87f5d93799d830a234f866ae62ef4af1328548fb
Shakila Marando <marandoshakila1@gmail.com>
2020-08-30 17:55:15 -0700

    final changes

--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,15 @@ router.get("/api/workouts", (req, res) => {
 });
 
 // TODO: change this one to a put request
-router.put("/api/workouts/:id", ({ body, params }, res) => {});
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  db.Workout.findOneAndUpdate(params.id, { $push: { exercises: body } })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 router.post("/api/workouts", ({ body }, res) => {
   db.Workout.create({})
@@ -27,6 +35,14 @@ router.post("/api/workouts", ({ body }, res) => {
 
 router.get("/api/workouts/range", function (req, res) {
   // TODO: get workouts range
+  db.Workout.find({})
+    .limit(5)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 module.exports = router;
